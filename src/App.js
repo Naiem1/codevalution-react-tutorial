4f32efd9c8a556baa7c86472df4418c0869dcb9a
naiem <naiem4288@gamil.com>
2020-12-14 20:42:50 +0600

    first-day react core part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+// import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="Bruce" heroName="Batman">
+        <p>This is Children props</p>
+       </Greet>
+      <Greet name="Clark" heroName='Superman'>
+        <button>Action</button>
+      </Greet>
+      <Greet name="Diana" heroName='Wonder Woman' />
+      <Welcome name="Bruce" heroName="Batman" />
+      <Welcome name="Clark" heroName="Superman"/>
+      <Welcome name="Diana" heroName="Wonder women"/>
+
+
+      {/* <Hello/> */}
     </div>
   );
 }
